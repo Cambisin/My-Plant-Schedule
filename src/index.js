c5877bf18ebf9bf401966b391855ed917f9fd7f7
Cameron Ewart <cameron_ewart17@hotmail.com>
2021-01-16 13:54:35 +0000

    calendar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Login } from './components/login.js';
+//import { Calendar } from './components/calendar.js';
 
 import "./styles.css";
 import { Navbar } from './components/Navbar.js';
@@ -17,14 +17,10 @@ function App() {
   return (
     <Container>
       <Navbar/>
+      <Calendar>
       <h1>Simple Calendar</h1>
-<<<<<<< HEAD
-      <Login />
-=======
-      <Calendar />
-      
->>>>>>> b2a3c787f4f70ae3a74d837fd2c9ba140d61477b
-    </Container>
+      </Calendar>
+      </Container>
   );
 }
 
